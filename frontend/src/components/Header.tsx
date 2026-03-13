b614ed6dac1b2531829e1f84f03d78a7775f4817
OldMoldyCake <79052622+oldmoldycake@users.noreply.github.com>
2026-03-12 22:07:55 -0500

    ="docs: Auto-updated documentation [skip ci]"

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,14 @@
-import { Grid, GridItem } from '@chakra-ui/react'
+=import { Grid, GridItem } from '@chakra-ui/react'
 import MenuDrawer from './MenuDrawer'
 export default function Header() {
   return (
     <Grid
-      h={"50px"}
-      templateRows={"repeat(1,1fr"}
-      templateColumns={"repeat(5, 1fr)"}
-      gap={4}
+      h{"50px"}
+      templateRows{"repeat(1,1fr"}
+      templateColumns{"repeat(5, 1fr)"}
+      gap{4}
     >
-      <GridItem rowSpan={1} colSpan={1} bgColor={"gray.600"}>
+      <GridItem rowSpan{1} colSpan{1} bgColor{"gray.600"}>
         <MenuDrawer />
       </GridItem>
 
